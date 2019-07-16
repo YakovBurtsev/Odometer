d2f53f02a44bcf50fcdabcd013528c8e5182723c
yakov <yakovburtsev@gmail.com>
2019-07-16 08:41:51 +0300

    implement OdometerService

--- app/src/main/java/com/example/odometer/OdometerService.java
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -2,15 +2,28 @@ package com.example.odometer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import java.util.Random;
+
 public class OdometerService extends Service {
-    public OdometerService() {
+
+    private final IBinder binder = new OdometerBinder();
+    private final Random random = new Random();
+
+    public class OdometerBinder extends Binder {
+        OdometerService getOdometer() {
+            return OdometerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    public double getDistance() {
+        return random.nextDouble();
     }
 }
